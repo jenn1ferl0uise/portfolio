597b0a519b04f85276aa192eb8dd1a38b3483c7c
Jennifer Lynch <jl.lynch9@gmail.com>
2019-03-29 19:15:13 +0100

    generated pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  get 'pages/contact'
+  get 'pages/about'
+  get 'pages/projects'
+  get 'pages/photography'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
