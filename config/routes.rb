2afa00a4bc1bd856a61627b703bc32cca6c5fb11
Jennifer Lynch <jl.lynch9@gmail.com>
2019-03-29 23:17:56 +0100

    created navbar with routes/links correctly set. removed photography page. basic home page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'pages/contact'
-  get 'pages/about'
-  get 'pages/projects'
-  get 'pages/photography'
+  get 'contact', to: 'pages#contact', as: 'contact'
+  get 'about', to: 'pages#about', as: 'about'
+  get 'projects', to: 'pages#projects', as: 'projects'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
